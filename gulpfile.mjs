105df80859fea0fcace44e4c22452a5610b8ceae
Rizwan Ali <webninjja@gmail.com>
2023-08-10 19:16:30 -0400

    fix: git bootstrap

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,29 +1,20 @@
-import gulp from 'gulp';
-import dartSass from 'sass';
-import gulpSass from 'gulp-sass';
-import csso from 'gulp-csso';
+import gulp from "gulp";
+import csso from "gulp-csso";
+import gulpSass from "gulp-sass";
+import dartSass from "sass";
 
 const { series, parallel, src, dest, task } = gulp;
-const sass = gulpSass( dartSass );
+const sass = gulpSass(dartSass);
 
 function scss() {
-
-	return src( 'public/assets/scss/magic-ai.scss' )
-		.pipe( sass.sync() )
-		.pipe( csso() )
-		.pipe( dest( 'public/assets/css' ) );
-
+  return src("public/assets/scss/magic-ai.scss")
+    .pipe(sass.sync())
+    .pipe(csso())
+    .pipe(dest("public/assets/css"));
 }
 
 export function watch() {
-	gulp.watch(
-        [ 'public/assets/scss/**/*.scss' ],
-		series(
-			parallel( scss )
-		)
-	);
+  gulp.watch(["public/assets/scss/**/*.scss"], series(parallel(scss)));
 }
 
-export const build = series(
-	parallel( scss )
-);
+export const build = series(parallel(scss));
